e995563f7f5e49b4816381d35a4c26ddab4d7f03
Gabriel <gmamanican@unsa.edu.pe>
2022-01-17 07:46:14 -0500

    Nuevo componente a renderizar para practicas

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { TodoApp } from './components/08-useReducer/TodoApp';
+import React from "react";
+import ReactDOM from "react-dom";
+// import { TodoApp } from "./components/08-useReducer/TodoApp";
 // // import { CallBackHook } from './components/06-memos/CallBackHook';
 // import { Padre } from './components/07-tarea-memo/Padre';
 // // import { MemoHook } from './components/06-memos/MemoHook';
@@ -12,10 +12,8 @@ import { TodoApp } from './components/08-useReducer/TodoApp';
 // // import { Memorize } from './components/06-memos/Memorize';
 // // import {CounterWithCustonHook} from './components/01-useState/CounterWithCustonHook';
 // // import { SimpleForm } from './components/02-useEffect/SimpleForm';
+import { MainApp } from "./components/09-useContext/MainApp";
 
-ReactDOM.render(
-    <TodoApp/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<MainApp />, document.getElementById("root"));
 
 // import "./components/08-useReducer/intro-reducer";
